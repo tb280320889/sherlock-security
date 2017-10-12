fed27a6088140e7c1b45e60f426ff4c66f2a6d07
tb280320889 <280320889@qq.com>
2017-10-12 17:22:57 +0800

    filePath optimized

--- sherlock-security-demo/src/main/java/com/github/tb280320889/security/demo/controller/FileController.java
+++ sherlock-security-demo/src/main/java/com/github/tb280320889/security/demo/controller/FileController.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.util.Date;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -15,23 +15,22 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by TangBin on 2017/10/12.
  */
 
-@Controller
+@RestController
 @RequestMapping("file")
 @Slf4j
 public class FileController {
 
-  private static String folder = "C:\\Users\\Tangbin\\codes\\sherlock-security\\sherlock-security-demo\\src\\main\\java\\com\\github\\tb280320889\\security\\demo\\controller";
 
   /**
    * @param file
@@ -46,7 +45,9 @@ public class FileController {
 
     final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
-    final File localfile = new File(folder, dateFormat.format(LocalDate.now()) + ".txt");
+    final String path = FileController.class.getResource("").getPath();
+    log.info("path : {} ", path);
+    final File localfile = new File(path, dateFormat.format(new Date()) + ".txt");
 
     //     file.getInputStream()
 
@@ -66,7 +67,7 @@ public class FileController {
   @GetMapping("/{id}")
   public void download(@PathVariable String id, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
 
-    try (FileInputStream fileInputStream = new FileInputStream(new File(folder, id + ".txt"));
+    try (FileInputStream fileInputStream = new FileInputStream(new File("", id + ".txt"));
          final ServletOutputStream outputStream = httpServletResponse.getOutputStream()
     ) {
       httpServletResponse.setContentType("application/x-download");
